1db158a8ecf80b1815ee2b9b083ac50341288da9
Peter M <pmoyano@solvd.com>
2023-01-30 16:52:20 -0300

    toggle ui

--- src/pages/1- useToggle/toggleComponent.js
+++ src/pages/1- useToggle/toggleComponent.js
@@ -5,10 +5,12 @@ export default function ToggleComponent() {
 
   return (
     <div>
-      <div>{}value.toString</div>
-      <button onClick={toggleValue}></button>)
-      <button onClick={() => toggleValue(true)}>Make True</button>)
-      <button onClick={() => toggleValue(false)}>Make False</button>)
+      <div>
+        <p>{value.toString()}</p>
+      </div>
+      <button onClick={toggleValue}>Toggle</button>
+      <button onClick={() => toggleValue(true)}>Make True</button>
+      <button onClick={() => toggleValue(false)}>Make False</button>
     </div>
   );
 }
